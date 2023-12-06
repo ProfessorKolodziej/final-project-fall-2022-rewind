c2cef74ac34d551d53ef52a8a50c082d6bcb7a83
florararay <junmany@bu.edu>
2023-12-06 18:23:21 -0500

    Updates

--- js/page4.js
+++ js/page4.js
@@ -1,5 +1,5 @@
 var marketButtonContainer = document.querySelector('.market-button-container');
 marketButtonContainer.addEventListener('click', function() {
-  var targetURL = 'file:///Users/junmanyang/Documents/GitHub/final-project-fall-2022-rewind/src/page3.html';
+  var targetURL = 'page3.html';
   window.location.href = targetURL;
 });
