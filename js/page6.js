906e14ee490ce20c45ea0c19ad6327785c52b422
florararay <junmany@bu.edu>
2023-12-06 18:25:53 -0500

    Updates

--- js/page6.js
+++ js/page6.js
@@ -2,11 +2,11 @@ var p62Button = document.querySelector('.back-to-map');
 var backto2023Button = document.querySelector('.back-to-2023');
 
 p62Button.addEventListener('click', function() {
-	var targetURL = 'file:///Users/junmanyang/Documents/GitHub/final-project-fall-2022-rewind/src/page3.html';
+	var targetURL = 'page3.html';
 	window.location.href = targetURL;
 });
 
 backto2023Button.addEventListener('click', function() {
-	var targetURL = '/Users/junmanyang/Documents/GitHub/final-project-fall-2022-rewind/src/page7-end.html';
+	var targetURL = 'page7-end.html';
 	window.location.href = targetURL;
 });
